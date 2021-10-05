471904858caec4f461091e58925e90a4ce283f79
bnguerrero <89376675+bnguerrero@users.noreply.github.com>
2021-10-05 14:15:43 -0700

    Update LinkedStack.java

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,7 +1,52 @@
-public class LinkedStack 
+import java.util.EmptyStackException;
+
+public class LinkedStack<T> implements StackInterface<T>
 {
-    
-  
-  
-  
+    private Node topNode; 
+
+    public LinkedStack()
+    {
+        topNode = null;
+    }
+    private class topNode
+    {
+        private T data; 
+        private Node next:
+
+
+    }
+
+    public void push(T newEntry)
+    {
+        Node newNode= new Node(newEntry, topNode);
+        topNode = newNode;
+    }
+
+    public T pop()
+    {
+        T top = peek();
+        topNode = topNode.getNextNode();
+        return top;
+    }
+
+    public T peek()
+    {
+        if (isEmpty())
+            throw new EmptyStackException();
+        else
+            return topNode.getData();
+    }
+
+    public boolean isEmpty()
+    {
+        return topNode == null;
+    }
+
+    public void clear()
+    {
+        topNode = null;
+    }
+
+
 }
+
