b175abeb0a5ab6f44920927cb468801e0e5aca03
bnguerrero <89376675+bnguerrero@users.noreply.github.com>
2021-10-17 00:39:09 -0700

    added test examples

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -2,8 +2,22 @@ public class LinkedStackTest
 {
     public static void main(String[] args) 
     {
-        StackInterface<String>  testStack = new LinkedStack<>();
-        System.out.println(testStack.convertToPostfix("a*b/(c-a)+d*e"));
+        StackInterface<String> testStack = new LinkedStack<>();
+
+        System.out.println("the postfix of a*b/(c-a)+d*e is: ");
+        testStack.convertToPostfix("a*b/(c-a)+d*e");
+
+        System.out.println("the postfix of a-b+c is: ");
+        testStack.convertToPostfix("a-b+c");
+
+        System.out.println("the postfix of a+b is: ");
+        testStack.convertToPostfix("a+b");
+
+        System.out.println("the postfix of (a+b)*c is: ");
+        testStack.convertToPostfix("(a+b)*c");
+
+        System.out.println("the postfix of a+b*c is: ");
+        testStack.convertToPostfix("a+b*c");
     }
 }
 
